93f2af321c6b21b16e600ff7f5ae3eab78d3399c
Gabriel Castelan <gabrielmcastelan@gmail.com>
2025-05-17 20:18:22 -0300

    boraaa

--- src/components/MessageDisplay.tsx
+++ src/components/MessageDisplay.tsx
@@ -1,6 +1,17 @@
 import { useEffect, useState } from "react";
 
-export const MessageDisplay = ({ message }: {message: {id: string; content: string; duration: number}}) => {
+export const MessageDisplay = (
+  { 
+    message, 
+    onMessageDeleted,
+  }: {
+    message: {
+      id: string;
+      content: string;
+      duration: number;
+    },
+    onMessageDeleted: () => void
+  }) => {
   const [timeLeft, setTimeLeft] = useState(message?.duration ?? 0);
 
   useEffect(() => {
@@ -11,10 +22,16 @@ export const MessageDisplay = ({ message }: {message: {id: string; content: stri
       setTimeLeft(prev => (prev > 0 ? prev - 1 : 0));
     }, 1000);
 
-    const timeout = setTimeout(() => {
-      fetch(`https://big-screen-backend.onrender.com/api/messages/${message.id}`, {
-        method: "DELETE",
+    const timeout = setTimeout(async () => {
+      await fetch(`https://big-screen-backend.onrender.com/api/messages/delete`, {
+        method: "POST",
+        body: JSON.stringify(
+          {
+            id: message.id
+          },
+        ),
       });
+      await onMessageDeleted();
     }, message.duration * 1000);
 
     return () => {
@@ -38,7 +55,7 @@ export const MessageDisplay = ({ message }: {message: {id: string; content: stri
       <p className="text-4xl md:text-6xl font-bold text-white leading-tight">
         {message.content}
       </p>
-      <div className="mt-4 text-2xl text-white/80">
+      <div className="mt-4 text-2xl text-white/80" style={{wordBreak: 'break-all'}}>
         Tempo restante: {timeLeft}s
       </div>
     </div>
--- src/pages/Telao.tsx
+++ src/pages/Telao.tsx
@@ -1,6 +1,7 @@
 
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { MessageDisplay } from "@/components/MessageDisplay";
+import qrCode from "@/assets/682916cf2947b.svg"
 
 // Types for our messages
 export interface Message {
@@ -13,17 +14,32 @@ export interface Message {
 const Telao = () => {
   const [messages, setMessages] = useState<any[]>([]);
 
-  useEffect(() => {
+  const loadMessages = useCallback(() => {
     fetch("https://big-screen-backend.onrender.com/api/messages")
-      .then(res => res.json())
-      .then(data => setMessages(data.messages ?? []))
+      .then((res) => res.json())
+      .then((data) => setMessages(data.messages ?? []))
       .catch(() => setMessages([]));
   }, []);
 
+  useEffect(() => {
+    loadMessages();
+  }, [loadMessages]);
+
+  useEffect(() => {
+  if (messages.length > 0) return;
+
+  const interval = setInterval(() => {
+    loadMessages();
+  }, 10000); // 30 segundos
+
+  return () => clearInterval(interval);
+}, [messages, loadMessages]);
+
   return (
     <div className="h-screen w-screen bg-black overflow-hidden relative">
-      <div className={`absolute inset-0 flex items-center justify-center transition-opacity duration-1000`}>
-        <MessageDisplay message={messages[messages.length - 1]} />
+      <div className={`absolute inset-0 flex flex-column items-center justify-center transition-opacity duration-1000`} style={{flexDirection: 'column'}}>
+        <MessageDisplay message={messages[messages.length - 1]} onMessageDeleted={loadMessages} />
+        <img src={qrCode} className="mt-8 w-60 h-60 object-contain"/>
       </div>
       <div className="absolute bottom-4 right-4 text-white/50 text-sm">
         <a href="http://swcriciuma.synapsee.com.br/">http://swcriciuma.synapsee.com.br/</a>
